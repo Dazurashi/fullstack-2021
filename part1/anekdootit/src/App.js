b63f18f1ea83c1984a1cd6d0b5813356e6775e69
Dazurashi <enaqueed@gmail.com>
2021-11-23 21:31:02 +0200

    anekdootit done

--- part1/anekdootit/src/App.js
+++ part1/anekdootit/src/App.js
@@ -1,5 +1,20 @@
 import React, { useState } from 'react'
 
+const Button = ({handleClick, text}) => {
+  return <button onClick={handleClick}>{text}</button>
+}
+
+const PopularVote = ({mostPopular, points}) => {
+  if (points === 0) {
+    return <p>No votes given yet</p>
+  }
+  return <p>{mostPopular} has {points} votes!</p>
+}
+
+const Anecdotes = ({ane, points}) => {
+  return <p>{ane} has {points} points</p>
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -12,10 +27,43 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+  const [popular, setPopular] = useState(selected)
+
+  //This also eliminates 2 or more same numbers appearing in a row
+  const setNumber = () => {
+    const randomNum = Math.floor(Math.random()*anecdotes.length)
+    setSelected(randomNum)
+    if (selected === randomNum) {
+      setNumber()
+    }
+    
+  }
+
+  const addVote = () => {
+    const copy = {...points}
+    copy[selected] += 1
+    setPoints(copy)
+    if (copy[selected] > points[popular]) {
+      setPopular(selected)
+      
+    }
+  }
 
   return (
     <div>
-      {anecdotes[selected]}
+      <h1>Anecdote of the day</h1>
+      <Anecdotes ane={anecdotes[selected]} points={points[selected]} />
+      
+      <div>
+        <Button handleClick={setNumber} text="next anecdote" />
+        <Button handleClick={addVote} text="vote" />
+      </div>
+      <h1>Anecdote with most votes</h1>
+        <PopularVote mostPopular={anecdotes[popular]} points={points[popular]} />
+      <div>
+        
+      </div>
     </div>
   )
 }
