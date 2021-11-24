ee8f3dd2c825d9bda19632b2b0a633331409e3fc
Dazurashi <enaqueed@gmail.com>
2021-11-24 16:59:43 +0200

    ???

--- part2/kurssitiedot/src/App.js
+++ part2/kurssitiedot/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+const Course = () => {
+  
+}
+
 const Header = (props) =>{
   return(
     <>
@@ -37,18 +41,22 @@ const Part = (props) =>{
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
