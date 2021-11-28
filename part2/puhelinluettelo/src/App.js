b1458b35374d939de8465744ca7f952a80ea8fc5
Dazurashi <enaqueed@gmail.com>
2021-11-28 17:51:00 +0200

    phonebook 2.10 done

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,47 +1,84 @@
 import React, { useState } from 'react'
 
+const Filter = ({newFind, handleFindChange}) => {
+  return <div>
+    filter shown with <input value={newFind} onChange={handleFindChange} />
+  </div>
+}
+
+const PersonForm = ({newName, newNum, handlePersonChange, handleNumChange, addPerson}) => {
+  return <form onSubmit={addPerson}>
+    <div>
+      name: <input value={newName} onChange={handlePersonChange}/>
+    </div>
+    <div>
+      number: <input value={newNum} onChange={handleNumChange}/>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+}
+
+const Persons = ({persons, newFind}) => {
+  return <ul>
+  {persons.filter(person => person.name.toUpperCase().includes(newFind.toUpperCase())).map(person =>
+    <li key={person.name}>
+      {person.name} {person.number}
+    </li>)}
+</ul>
+}
+
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [newNum, setNewNum] = useState("")
+  const [newFind, setNewFind] = useState("")
+
   const addPerson = (event) => {
     event.preventDefault()
     console.log("Nappia painettu", event.target)
     const personObject = {
       name: newName,
+      number: newNum
     }
     if (persons.filter(person => person.name === personObject.name).length) {
       window.alert(`${newName} is already added to phonebook`)
       setNewName("")
+      setNewNum("")
     }else{
       setPersons(persons.concat(personObject))
       setNewName("")
+      setNewNum("")
     }
   }
+
   const handlePersonChange = (event) => {
     //console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumChange = (event) => {
+    setNewNum(event.target.value)
+  }
+
+  const handleFindChange = (event) => {
+    setNewFind(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handlePersonChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter newFind={newFind} handleFindChange={handleFindChange}/>
+      <h2>add a new</h2>
+      <PersonForm newName={newName} newNum={newNum} handlePersonChange={handlePersonChange} handleNumChange={handleNumChange} addPerson={addPerson} />
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person =>
-          <li key={person.name}>
-            {person.name}
-          </li>)}
-      </ul>
+      <Persons persons={persons} newFind={newFind} />
     </div>
   )
 
