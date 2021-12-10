ab14eae16d623944ea48b1defcc760a769e2ae45
Dazurashi <enaqueed@gmail.com>
2021-12-10 17:36:31 +0200

    maat ei toimi ja pitää erotella komponentteihin

--- part2/kurssitiedot/src/components/Course.js
+++ part2/kurssitiedot/src/components/Course.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+//TODO laita nää eri komponentteihin
 const Course = (props) => {
     console.log(props)
 
--- part2/maidentiedot/src/App.js
+++ part2/maidentiedot/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react"
+import axios from "axios"
 
-function App() {
+const Filter = (props) => {
+  return <div>
+    find countries <input />
+  </div>
+}
+
+const Countries = ({countries, search}) => {
+  const [visible, setVisible] = useState()
+
+  const showDetails = event => {
+    console.log(event.target.value)
+    const maa = countries.filter(country =>
+      country.name.includes(event.target.value))
+      console.log("maa: ", maa)
+      setVisible(maa[0])
+  }
+
+  const list = countries.filter(country => 
+    country.name.toUpperCase().includes(search.toUpperCase())
+  )
+  
+  
+
+  if(list.length > 10) {
+    return <p>Too many matches, specify another filter</p>
+  }
+  if(visible !== undefined) {
+    return (
+      <Country key={visible.name} name={visible.name} capital={visible.capital} />
+    )
+  }
+  if(list.length > 1){
+    return(
+      <ul>
+        {countries.filter(country => country.name.toUpperCase().includes(search.toUpperCase()).map(country => 
+        <List key={country.name} name={country.name} country={country} showDetails={showDetails}/>))}
+      </ul>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {countries.filter(country => country.name.toUpperCase().includes(search.toUpperCase())).map(country => (
+        <Country key={country.name} name={country.name} capital={country.capital}/>
+      ))}
+    </ul>
+  )
+}
+
+const Country = ({name, capital}) => {
+  return <div>
+    <h1>{name}</h1>
+    capital {capital}
+  </div>
+}
+
+const List = ({name, country, showDetails}) => {
+  return (
+    <li key={name}> 
+    {name}
+    <button value={country.name} onClick={showDetails}>show</button>
+    </li>
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState("")
+
+  const handleSearchChange = event => {setSearch(event.target.value)}
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then(response =>
+    setCountries(response.data))
+  }, [])
+
+  return (
+    <div>
+      <Filter search={search} handleSearchChange={handleSearchChange}/>
+      <Countries countries={countries} search={search}/>
     </div>
-  );
+  )
 }
 
 export default App;
