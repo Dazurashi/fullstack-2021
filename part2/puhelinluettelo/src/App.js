91dfaf2da325006b2e148e5fe68b837fbeb2cc1c
Dazurashi <enaqueed@gmail.com>
2021-12-15 17:42:38 +0200

    added several funtions

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import axios from "axios"
+import personService from "./services/persons"
+
+//TODO laita komponentit kansioihin ja tee index.css kuten tehtävässä pyydetään. Siisti myös koodia
 
 const Filter = ({newFind, handleFindChange}) => {
   return <div>
@@ -21,13 +23,30 @@ const PersonForm = ({newName, newNum, handlePersonChange, handleNumChange, addPe
   </form>
 }
 
-const Persons = ({persons, newFind}) => {
-  return <ul>
-  {persons.filter(person => person.name.toUpperCase().includes(newFind.toUpperCase())).map(person =>
-    <li key={person.name}>
-      {person.name} {person.number}
-    </li>)}
-</ul>
+const Persons = ({persons, newFind, handleDeletePerson}) => {
+
+  return (
+    <ul>
+      {persons.filter(person => person.name.toUpperCase().includes(newFind.toUpperCase())).map(person => (
+        <UniquePerson key={person.id} name={person.name} num={person.number} deletePerson={handleDeletePerson(person.name, person.id)}/> 
+      ))}
+    </ul>
+  )
+}
+
+const Notification = ({msg}) => {
+  if (msg === null){
+      return null
+  }
+  return (
+      <div className="error"> {msg}</div>
+  )
+}
+
+const UniquePerson = ({name, num, deletePerson}) =>{
+  return (
+<li>{name} {num} <button onClick={deletePerson}> delete</button></li>
+  )
 }
 
 const App = () => {
@@ -41,29 +60,51 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [newNum, setNewNum] = useState("")
   const [newFind, setNewFind] = useState("")
+  const [notification, setNotification] = useState(null)
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then(response => {
-      setPersons(response.data)
+    personService.getAll().then(response => {
+      setPersons(response)
     })
-
-  })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
-    console.log("Nappia painettu", event.target)
     const personObject = {
       name: newName,
       number: newNum
     }
-    if (persons.filter(person => person.name === personObject.name).length) {
-      window.alert(`${newName} is already added to phonebook`)
-      setNewName("")
-      setNewNum("")
-    }else{
-      setPersons(persons.concat(personObject))
-      setNewName("")
-      setNewNum("")
+    if (persons.filter(person => person.name === personObject.name).length > 0){
+      if (window.confirm(`${personObject.name} is already added to phonebook, replace the old number with a new one?`)) {
+        const oldPerson = persons.find(person => person.name === newName)
+        personService.update(oldPerson.id, {...oldPerson, number: newNum}).then(newPerson => {
+          setPersons(persons.map(person => (person.name === newName ? newPerson : person)))
+        }).catch(error => {
+          setNotification(`Information of ${personObject.name} has already been removed from server`)
+          console.log("Failed to update")
+        })
+        setPersons(persons.concat(personObject))
+        setNotification(`Number of ${personObject.name} has been updated`)
+        setNewName("")
+        setNewNum("")
+        setTimeout(() => {
+          setNotification(null)
+        }, 5000)
+      }
+    } 
+    else{
+      personService.addPerson(personObject).then(person => {
+        setPersons(persons.concat(person))
+        setNotification(`Added ${personObject.name}`)
+        setNewName("")
+        setNewNum("")
+      }).catch(error => {
+        setNotification(error.response.data.error)
+        console.log(error.response.data)
+      })
+      setTimeout(() => {
+        setNotification(null)
+      }, 5000)
     }
   }
 
@@ -80,14 +121,33 @@ const App = () => {
     setNewFind(event.target.value)
   }
 
+  const handleDeletePerson = (name, id) => {
+    return () => {
+      if (window.confirm(`Delete ${name} ?`)) {
+        personService.deletePerson(id).then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+          setNotification(`Deleted ${name}`)
+        }).catch(error => {
+          setPersons(persons.filter(person => person.id !== name))
+          setNotification(`Person named ${name} has already been deleted`)
+        })
+        setTimeout(() => {
+          setNotification(null)
+        }, 5000)
+      }
+    }
+
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification msg={notification}/>
       <Filter newFind={newFind} handleFindChange={handleFindChange}/>
       <h2>add a new</h2>
       <PersonForm newName={newName} newNum={newNum} handlePersonChange={handlePersonChange} handleNumChange={handleNumChange} addPerson={addPerson} />
       <h2>Numbers</h2>
-      <Persons persons={persons} newFind={newFind} />
+      <Persons persons={persons} newFind={newFind} handleDeletePerson={handleDeletePerson}/>
     </div>
   )
 
