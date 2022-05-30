58edbc8466d3a8553329068ebb8d14c64c50538c
Dazurashi <enaqueed@gmail.com>
2022-05-30 15:07:32 +0300

    4.14 done

--- part4/blogilista/controllers/blogs.js
+++ part4/blogilista/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('../app')
 const Blog = require('../models/blog')
 
 
@@ -31,6 +32,23 @@ blogsRouter.post('/', async (request, response) => {
 blogsRouter.delete('/:id', async (request, response) => {
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        author: body.author,
+        title: body.title,
+        url: body.url,
+        likes: body.likes}
+        
+    const updated = await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+    if(updated) {
+        response.status(200).json(updated.toJSON())
+    }else {
+        response.status(404).end()
+    }
 })
 
 module.exports = blogsRouter
